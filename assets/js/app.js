78bcd71ca77ddd213d7dc658c474ade1d0487d26
Karim El Safy <karim.elsafy@zkteco.com>
2015-04-28 18:48:22 +0300

    adding php sending mail

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,23 @@ var AboutController = function($scope, $http, $sce){
 var ContactController = function($scope, $http, $sce){
 	$scope.sendMsg = function(contactForm) {
 		if(contactForm.$valid){
-        	console.log($scope.email);
+        	$http({
+                method  : 'POST',
+                url     : 'backend/contact-form.php',
+                data    : $.param($scope.formData),  //param method from jQuery
+                headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  //set the headers so angular passing info as form data (not request payload)
+            }).success(function(data){
+                console.log(data);
+                if (data.success) { //success comes from the return json object
+                    $scope.submitButtonDisabled = true;
+                    $scope.resultMessage = data.message;
+                    $scope.result='bg-success';
+                } else {
+                    $scope.submitButtonDisabled = false;
+                    $scope.resultMessage = data.message;
+                    $scope.result='bg-danger';
+                }
+            });
 		}
     }	
 };
